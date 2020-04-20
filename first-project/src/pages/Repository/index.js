87d8e163e45ebe2d7236519a547bd12207a1eb87
Sadosco <sadoscolima@gmail.com>
2020-04-20 11:06:43 -0300

    routes navigation

--- first-project/src/pages/Repository/index.js
+++ first-project/src/pages/Repository/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-export default function Repository() {
-  return <h1>Respository</h1>;
+export default function Repository({ match }) {
+  return <h1>Respository: {decodeURIComponent(match.params.repository)}</h1>;
 }
